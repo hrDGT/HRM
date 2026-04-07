3b168635a7cc902cc290d1246cb9832c3054d19a
Stanislau Vyrski <vyr4376@yandex.ru>
2026-04-07 23:00:22 +0300

    refactor: added the eslint package for clean imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,11 +14,32 @@ const eslintConfig = defineConfig([
     "dist/**",
     "node_modules/**",
     "playwright-report/**",
-    "__tests__/**",
-    "e2e/**",
+    // "__tests__/**",
+    // "e2e/**",
     "blob-report/**",
     "next-env.d.ts",
   ]),
+
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^next", "^@?\\w"],
+            ["^@/components", "^@/lib", "^@/hooks", "^@/utils", "^@/"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            ["^.+\\.?(css|scss|sass)$"],
+          ],
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
