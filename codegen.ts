34c823ddc8670ac7b9127f4b4a84d92674b83b32
Stanislau Vyrski <vyr4376@yandex.ru>
2026-04-07 22:39:08 +0300

    feat: the codegen configuration has been modified

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,11 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   schema: 'http://localhost:3001/api/graphql',
 
-  documents: ['lib/**/*.ts', 'app/**/*.tsx'],
+  documents: [
+    'lib/**/*.ts',
+    'app/**/*.tsx',
+    'components/**/*.{ts,tsx}'
+  ],
 
   generates: {
     './gqlcodegen/': {
