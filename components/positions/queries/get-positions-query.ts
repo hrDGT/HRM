980a3d8908f1762df457f9396cf07be28b0988c0
Stanislau Vyrski <vyr4376@yandex.ru>
2026-04-15 20:59:22 +0300

    feat: actions and requests have been created for the “positions” page

---
+++ components/positions/queries/get-positions-query.ts
@@ -0,0 +1,27 @@
+import { cacheLife, cacheTag } from 'next/cache';
+
+import { gqlRequestAuthed } from '@/lib/gql/graphql-client';
+import { graphql } from "@/gqlcodegen";
+
+export const GET_POSITIONS_QUERY = graphql(`
+  query GetPositions {
+    positions {
+      id
+      name
+    }
+  }
+`);
+
+export async function fetchPositions(token?: string) {
+  'use cache'
+  cacheLife('hours')
+  cacheTag('positions')
+
+  const response = await gqlRequestAuthed(
+    GET_POSITIONS_QUERY,
+    undefined,
+    { token }
+  )
+
+  return response.positions
+}
